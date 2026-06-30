ec6acee07cec1fe7528de1da2d4a97e628d7e707
NicoFSPD <linegarcina@gmail.com>
2026-06-30 01:23:07 -0400

    feat: Implementacion de controles del manejo de reservaciones

--- src/main/java/TarFinal/graphics/PanelControl.java
+++ src/main/java/TarFinal/graphics/PanelControl.java
@@ -3,4 +3,38 @@ package TarFinal.graphics;
 import javax.swing.*;
 
 public class PanelControl extends JPanel {
+
+    private JPanel panelAcciones;
+
+    /**Constructor de la clase*/
+    public PanelControl(){
+
+        JButton btnReservar = new JButton("Crear reservacion");
+        JButton btnModificar = new JButton("Modificar reservacion");
+        JButton btnAnular = new JButton("Anular reservacion");
+
+        this.panelAcciones = new JPanel();
+
+        panelAcciones.setLayout(new GridLayout(3, 1, 5, 5));
+        panelAcciones.setBounds(60, 60, 160, 100);
+        panelAcciones.setBackground(Color.CYAN);
+        panelAcciones.add(btnReservar);
+        panelAcciones.add(btnModificar);
+        panelAcciones.add(btnAnular);
+
+        add(panelAcciones);
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        //Literalmente, el repintado del apartado de botones de esta seccion
+        panelAcciones.setBounds(60, 60, 160, 100);
+        panelAcciones.revalidate();
+        panelAcciones.repaint();
+    }
+
+
 }
