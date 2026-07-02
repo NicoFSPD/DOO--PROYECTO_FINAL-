5d8a90340042df62a7f212c6d651147fd5e33b7d
Daniel Cristobal Patricio Lopez Ramirez <lopezramirezdaniel946@gmail.com>
2026-07-01 21:46:11 -0400

    feat: Conectaxión de PanelTutores con ProxyTutorActual e integración de ButtonGroup.

--- src/main/java/TarFinal/graphics/PanelTutores.java
+++ src/main/java/TarFinal/graphics/PanelTutores.java
@@ -1,10 +1,13 @@
 package TarFinal.graphics;
 
+import TarFinal.controlador.ProxyTutorActual;
 import TarFinal.logica.GestorReservas;
 import TarFinal.logica.Tutor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class PanelTutores extends JPanel {
 
@@ -14,14 +17,27 @@ public class PanelTutores extends JPanel {
 
     public PanelTutores(GestorReservas gestor){
         this.gestor = gestor;
+        this.setLayout(null);
 
         this.panelOpciones = new JPanel(new GridLayout(5,1,5,5));
 
         panelOpciones.setBounds(60,60,150,150);
 
+        ButtonGroup grupo = new ButtonGroup();
+
         for(Tutor t: gestor.getTutores()){
             String nombre = t.getNombre();
-            panelOpciones.add(new JRadioButton(nombre));
+            JRadioButton boton = new JRadioButton(nombre);
+
+            boton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    ProxyTutorActual.getInstancia().setTutorSeleccionado(t);
+                }
+            });
+
+            grupo.add(boton);
+            panelOpciones.add(boton);
         }
 
         add(panelOpciones);
