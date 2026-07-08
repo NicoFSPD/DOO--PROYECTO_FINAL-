f6db5799a5b76aa706270506f5b384fb19878422
NicoFSPD <linegarcina@gmail.com>
2026-07-07 23:26:32 -0400

    feat: Comienzo de las pruebas unitarias

---
+++ src/test/java/TutoriasTest.java
@@ -0,0 +1,25 @@
+import TarFinal.logica.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TutoriasTest {
+
+    /// Crear todas las intancias necesarias para los tests
+
+    private GestorReservas gestor;
+    private BloqueHorario bloque;
+    private Estudiante est1;
+    private Estudiante est2;
+    private Tutor tut1;
+    private Tutor tut2;
+    private Reserva reservacion;
+    private Materia ramo;
+
+
+}
